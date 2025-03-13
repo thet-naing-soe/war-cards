98c3869a188edd1764e63823928997076ed3a073
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-03-13 23:40:20 +0630

    Display our card images

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let deckId;
 const button = document.getElementById("new-deck");
+const draw = document.getElementById("new-draw");
+const cards = document.getElementById("cards");
+
 function handleClick() {
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then((res) => res.json())
@@ -10,3 +13,14 @@ function handleClick() {
 }
 
 button.addEventListener("click", handleClick);
+draw.addEventListener("click", () => {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.cards);
+      cards.innerHTML = `
+        <img src=${data.cards[0].image} />
+        <img src=${data.cards[1].image} />
+      `;
+    });
+});
